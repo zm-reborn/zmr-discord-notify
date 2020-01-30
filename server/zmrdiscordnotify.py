0c0fa1f8e79a45c0d4ba89234a7a67148a0bc6a1
TotallyMehis <totally.mehis@gmail.com>
2020-01-30 17:45:33 +0200

    pep8 like code

--- server/zmrdiscordnotify.py
+++ server/zmrdiscordnotify.py
@@ -13,6 +13,7 @@ from configparser import ConfigParser
 def escape_everything(data):
     return discord.utils.escape_markdown(discord.utils.escape_mentions(data))
 
+
 def get_valid_tokens():
     tokens = []
     with open(path.join(path.dirname(__file__), '.tokens.txt')) as fp:
@@ -42,6 +43,7 @@ class RequestData:
         self.max_players = int(data['max_players'])
         self.player_name = escape_everything(data['player_name'])
 
+
 class MyDiscordClient(discord.Client):
     def __init__(self, config):
         super().__init__()
@@ -70,6 +72,8 @@ class MyDiscordClient(discord.Client):
         try:
             print('Initializing HTTP server...')
 
+            sslcontext = None
+            if self.cert_path or self.key_path:
             sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
             sslcontext.load_cert_chain(self.cert_path, self.key_path)
 
@@ -78,21 +82,19 @@ class MyDiscordClient(discord.Client):
             site = web.TCPSite(runner, port=self.port, ssl_context=sslcontext)
             print('Starting HTTP server...')
             await site.start()
-            print('Done with HTTP server...')
         except Exception as e:
             print(e)
+        else:
+            print('Done with HTTP server...')
 
-
-    #curl -X POST -H "Content-Type: application/json" -d @request_test1.txt localhost:3000
     """Handles the POST request from servers"""
     async def handle_webrequest(self, request):
-        #print(await request.text())
         data = None
         try:
             d = await request.json()
             data = RequestData(d, self.token)
-        except:
-            print('Error occurred when parsing json from request!')
+        except Exception as e:
+            print('Error occurred when parsing json from request!', e)
             print('Body:')
             print(await request.text())
         
@@ -101,7 +103,10 @@ class MyDiscordClient(discord.Client):
 
         try:
             print('Sending mention!')
-            await self.my_channel.send(content=self.format_content(data), embed=self.format_embed(data))
+            await self.my_channel.send(
+                content=self.format_content(data),
+                embed=self.format_embed(data)
+            )
         except Exception as e:
             print(e)
 
@@ -112,7 +117,8 @@ class MyDiscordClient(discord.Client):
 
         self.my_channel = self.get_channel(self.channel_id)
         if self.my_channel is None:
-            raise Exception('Channel with id %i does not exist!' % self.channel_id)
+            raise Exception('Channel with id %i does not exist!' %
+                            self.channel_id)
         
         self.my_guild = self.my_channel.guild
 
@@ -128,7 +134,8 @@ class MyDiscordClient(discord.Client):
         if message.author == self.user:
             return
         # Only either in my channel or DM
-        if message.channel != self.my_channel and not isinstance(message.channel, discord.DMChannel):
+        if message.channel != self.my_channel
+        and not isinstance(message.channel, discord.DMChannel):
             return
         # Not a member of my server
         member = self.my_guild.get_member(message.author.id)
@@ -146,14 +153,18 @@ class MyDiscordClient(discord.Client):
 
 
     def format_content(self, data):
-        desc = '%s **%s** wants you to join! (*%i*/*%i*)' % (self.my_ping_role.mention, data.player_name, data.num_players, data.max_players)
+        desc = '%s **%s** wants you to join! (*%i*/*%i*)' %
+        (self.my_ping_role.mention,
+            data.player_name,
+            data.num_players,
+            data.max_players)
         return desc
 
     def format_embed(self, data):
         name = data.hostname
         link = data.link
 
-        embed=discord.Embed(title=name, description=link, color=0x13e82e)
+        embed = discord.Embed(title=name, description=link, color=0x13e82e)
         return embed
 
     #
@@ -162,14 +173,17 @@ class MyDiscordClient(discord.Client):
     async def add_ping_role(self, member, from_channel):
         if self.my_ping_role in member.roles:
             try:
-                await from_channel.send("%s You already have role %s!" % (member.mention, self.my_ping_role.name))
+                await from_channel.send("%s You already have role %s!" %
+                                        (member.mention,
+                                            self.my_ping_role.name))
             except Exception as e:
                 print(e)
             return
         try:
             print('Adding role to user!')
             await member.add_roles(self.my_ping_role, reason='User requested.')
-            await from_channel.send('%s Added role %s.' % (member.mention, self.my_ping_role.name))
+            await from_channel.send('%s Added role %s.' %
+                                    (member.mention, self.my_ping_role.name))
         except Exception as e:
             print(e)
 
@@ -177,16 +191,22 @@ class MyDiscordClient(discord.Client):
     # Remove ping role
     #
     async def remove_ping_role(self, member, from_channel):
-        if not self.my_ping_role in member.roles:
+        if self.my_ping_role not in member.roles:
             try:
-                await from_channel.send("%s You don't have role %s!" % (member.mention, self.my_ping_role.name))
+                await from_channel.send("%s You don't have role %s!" %
+                                        (member.mention,
+                                            self.my_ping_role.name))
             except Exception as e:
                 print(e)
             return
         try:
             print('Removing role from user!')
-            await member.remove_roles(self.my_ping_role, reason='User requested.')
-            await from_channel.send('%s Removed role %s.' % (member.mention, self.my_ping_role.name))
+            await member.remove_roles(
+                self.my_ping_role,
+                reason='User requested.')
+            await from_channel.send('%s Removed role %s.' %
+                                    (member.mention,
+                                        self.my_ping_role.name))
         except Exception as e:
             print(e)
 
